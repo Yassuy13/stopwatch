d59568ca352206be26960e9adfe7b83bd754370c
EC2 Default User <ec2-user@ip-172-31-94-2.ec2.internal>
2022-02-20 10:06:43 +0000

    second commit

--- main.js
+++ main.js
@@ -5,29 +5,52 @@ let addition = 0;
 const record = document.querySelector(".counter");
 // カウンター
 function timer() {
-const start = new Date().getTime();
-stop = setInterval(function() {
-progress = new Date().getTime() - start + addition;
-const noms = progress / 1000;
-const millisecond = progress ? ("0" + String(noms).split(".")[1]).slice(-1) : "0";
-const nos = Math.trunc(noms);
-const second = nos ? ("0" + (nos % 86400 % 3600 %60)).slice(-2) : "0";
-const minute = nos >= 60 ? ("0" + Math.trunc(nos % 86400 % 3600 / 60)).slice(-2) : "0";
-const hour = nos >= 360 ? ("0" + Math.trunc(nos % 86400 / 3600)).slice(-2) : "0";
-if (progress < 86400) {
-record.textContent = hour + ":" + minute + ":" + second + ":" + millisecond; } else {
-record.textContent = "0:0:0:0"; clearInterval(stop); }}, 10); }
+  const start = new Date().getTime();
+  stop = setInterval(function() {
+    progress = new Date().getTime() - start + addition;
+    const noms = progress / 1000;
+    const millisecond = progress ? (String(noms).split(".")[1]).slice(-1) : "0";
+    const nos = Math.trunc(noms);
+    const second = millisecond ? ("0" + (nos % 3600 % 60)).slice(-2) : "0";
+    const minute = second ? ("0" + Math.trunc(nos % 3600 / 60)).slice(-2) : "0";
+    const hour = minute ? ("0" + Math.trunc(nos / 3600 % 60)).slice(-2) : "0";
+      if (progress) {
+      record.textContent = hour + ":" + minute + ":" + second + ":" + millisecond;
+    } else {
+      record.textContent = "0:0:0:0";
+      clearInterval(stop);
+    }
+  });
+}
 // ボタン部
 const startButton = document.querySelector("button.start");
 const stopButton = document.querySelector("button.stop");
 const resetButton = document.querySelector("button.reset");
-stopButton.disabled = true; resetButton.disabled = true;
+stopButton.disabled = true;
+resetButton.disabled = true;
 // スタート
 startButton.addEventListener("click", function() {
-progress = 0; timer(); startButton.disabled = true; stopButton.disabled = false; resetButton.disabled = false; });
+  progress = 0;
+  timer();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = false;
+});
 // ストップ
 stopButton.addEventListener("click", function() {
-clearInterval(stop); addition = progress; startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = false; });
+  clearInterval(stop);
+  addition = progress;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+});
 // リセット
 resetButton.addEventListener("click", function() {
-clearInterval(stop); progress = 0; record.textContent = "0:0:0:0"; addition = 0; startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = true; });
+  clearInterval(stop);
+  progress = 0;
+  record.textContent = "0:0:0:0";
+  addition = 0;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+});
